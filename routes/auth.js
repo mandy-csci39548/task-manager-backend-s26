b4dcf9a46097f4e3b2fb6af58dffe900b537fcae
Mandy <mandy39548@gmail.com>
2026-04-15 18:48:58 -0400

    login endpoint added

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import express from 'express'
 import { PrismaClient } from '../generated/prisma/client.ts'
@@ -29,6 +30,29 @@ router.post('/register', async (req, res) => {
   })
 
   res.json(user)
+})
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (!user) {
+    return res.status(401).json({ error: 'User not found' })
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) {
+    return res.status(401).json({ error: 'Invalid password ' })
+  }
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: '5s',
+  })
+
+  res.json({ token })
 })
 
 export default router
