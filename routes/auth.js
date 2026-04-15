a95dba5e7678801988d1de0314228554eae47c8c
Mandy <mandy39548@gmail.com>
2026-04-15 18:26:55 -0400

    register endpoint added

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import express from 'express'
 import { PrismaClient } from '../generated/prisma/client.ts'
 import { PrismaPg } from '@prisma/adapter-pg'
@@ -6,5 +7,28 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
 
 const router = express.Router()
+
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (existingUser) {
+    return res.status(400).json({ error: 'Email already registered. ' })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  res.json(user)
+})
 
 export default router
